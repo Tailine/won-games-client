113947fe280b3ae6586bc641b40a628b004741ff
Tailine Chagas <tailinechagas@gmail.com>
2021-05-12 08:49:40 -0300

    Initial commit from Create Next App

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testPathIgnorePatterns: ['/node_modules', '/next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
