6522b423d4b15a251494aa41d10ab73ad2267eab
Tailine Chagas <tailinechagas@gmail.com>
2021-06-12 17:16:26 -0300

    feat: implements banner component

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+
+import Banner from '.'
+import { renderWithTheme } from '../../utils/tests/helpers'
+
+const defaultProps = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...defaultProps} />)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: 'Play the new CrashLands season' })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
